c1a66eeba6033d705b0f93897d6654c265051caa
luisedsilva <135769052+luisedsilva@users.noreply.github.com>
2024-07-08 18:41:39 -0300

    Final

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express'); 
+const express = require('express');
 const path = require('path');
-const expressLayouts = require('express-ejs-layouts')
-const session = require('express-session')
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
 const tarefaController = require('./controllers/tarefaController');
 const usuarioController = require('./controllers/usuarioController'); 
 const app = express(); 
 const port = 3000; 
 
-app.use(session({secret:'i1n2f3o4'}));
+app.use(session({ secret: 'i1n2f3o4', resave: false, saveUninitialized: true }));
+
 app.set('view engine', 'ejs'); 
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
@@ -23,21 +24,32 @@ app.use((req, res, next) => {
     };
     next();
 });
-app.get('/', (req, res)=>{res.send("<h1>API Tarefas</h1>")});
-app.use(express.urlencoded({ extended: true })); 
+
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to check if user is authenticated
+function isAuthenticated(req, res, next) {
+    if (req.session.user) {
+        return next();
+    }
+    return res.redirect('/login');
+}
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', usuarioController.login);
+app.post('/Usuario/autenticar', usuarioController.autenticar);
+
+// Applying the isAuthenticated middleware to all /tarefas routes
+app.use('/tarefas', isAuthenticated);
+
 app.get('/tarefas', tarefaController.getTarefas); 
 app.post('/tarefas', tarefaController.addTarefa); 
 app.get('/tarefas/delete/:id', tarefaController.deleteTarefa);
-app.get('/login', usuarioController.login);
-app.post('/Usuario/autenticar', usuarioController.autenticar);
-app.get('/tarefas', (req, res)=>{
-  if(req.session.user){
-    tarefaController.getTarefas(req,res);
-  }else{
-    return res.redirect('/login');
-  }
-});
- 
+
 app.listen(port, () => { 
-console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
